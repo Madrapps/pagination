a8e67569df06f5ecd2eef16bef647ac9fa8e3db9
thsaravana <th.saravana@gmail.com>
2015-08-30 12:38:51 +0530

    View created

---
+++ src/main/java/com/instrap/apps/pagination/views/MovingDots.java
@@ -0,0 +1,22 @@
+package com.instrap.apps.pagination.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by tsaravana on 8/30/2015.
+ */
+public class MovingDots extends View{
+    public MovingDots(Context context) {
+        super(context);
+    }
+
+    public MovingDots(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MovingDots(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
